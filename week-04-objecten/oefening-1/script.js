4c3d91436da34ddc22e01adf80a2a0a1f7b9bd03
Jason <j.vanschaik@tcrmbo.nl>
2026-04-14 14:45:37 +0200

    Update script.js

--- week-04-objecten/oefening-1/script.js
+++ week-04-objecten/oefening-1/script.js
@@ -3,13 +3,13 @@ const car = {
   brand: 'Volkswagen',
   year: 2008,
   mileage: 145000,
-  description: function() {
+  description() {
     // Vul in: geef een zin terug met this.name en this.brand via template literal
   },
-  isOld: function() {
+  isOld() {
     // Vul in: geef true terug als het year voor 2010 is
   },
-  drive: function(km) {
+  drive(km) {
     // Vul in: verhoog this.mileage met km en geef de nieuwe km-stand terug
   },
 };
