c846f6084759f31f626eb2e89dc1d41103e74d1a
Jason <j.vanschaik@tcrmbo.nl>
2026-04-14 16:42:18 +0200

    minor fix

--- week-05-herhaling/oefening-2/script.js
+++ week-05-herhaling/oefening-2/script.js
@@ -2,13 +2,12 @@ const defaults = { name: '', score: 0, level: 1 };
 
 const players = [];
 
-const list = document.querySelector('#players');
+const playersList = document.querySelector('#players');
 
 const showPlayers = () => {
-  list.innerHTML = players.map(s => `<li>${s.name} — ${s.score} punten</li>`).join('');
+  playersList.innerHTML = players.map(player => `<li>${player.name} — ${player.score} punten</li>`).join('');
 };
 
 // Luister naar het submit-event op #player-form
-// Valideer: naam mag niet leeg zijn, score moet een getal zijn
-// Maak een nieuw speler-object aan met spread op defaults en de ingevulde waarden
+// Maak een nieuw player object aan met spread op defaults en de ingevulde waarden
 // Voeg het toe aan de array met push en herrender de lijst
