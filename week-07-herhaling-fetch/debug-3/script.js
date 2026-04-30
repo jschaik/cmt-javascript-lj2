a9ec3891b2256e1073cd0cc2ab557aca337e8c0e
Jason <j.vanschaik@tcrmbo.nl>
2026-04-30 09:48:24 +0200

    debug

--- week-07-herhaling-fetch/debug-3/script.js
+++ week-07-herhaling-fetch/debug-3/script.js
@@ -1,14 +1,14 @@
 // Fout gevonden: _______________
 // Oplossing: _______________
 
-fetch('./users.json')
+fetch('./users')
   .then(r => r.json())
   .then(users => {
-    const list = document.querySelector('#results');
+    const list = document.querySelector('#result');
     users.forEach(user => {
       const li = document.createElement('li');
       li.textContent = user.name;
-      list.appendChild(li);
+      list.appendChild(list);
     });
   })
   .catch(error => console.error('Fout:', error));
